b9b41e8ce055669029f38948fe3f3f3930afa31d
zouyos <hamza.benketaf@gmail.com>
2024-08-22 12:08:40 +0200

    loggedIn stored in localStorage

--- src/store/auth/auth-slice.js
+++ src/store/auth/auth-slice.js
@@ -5,7 +5,8 @@ export const authSlice = createSlice({
   name: 'authSlice',
   initialState: {
     loggedIn:
-      !!Cookies.get('token') && JSON.parse(localStorage.getItem('loggedIn')),
+      !!Cookies.get('token') &&
+      JSON.parse(localStorage.getItem('loggedIn') || 'false'),
     serverErrors: [],
   },
   reducers: {
