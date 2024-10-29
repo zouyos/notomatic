8e4ef72db395e89b98c1a78fcb204192670a6f44
zouyos <hamza.benketaf@gmail.com>
2024-10-29 13:08:47 +0100

    update

--- src/store/note/note-slice.ts
+++ src/store/note/note-slice.ts
@@ -4,14 +4,14 @@ import { createSlice } from "@reduxjs/toolkit";
 export const noteSlice = createSlice({
   name: "noteSlice",
   initialState: {
-    noteList: [] as any[],
+    noteList: [] as NoteType[],
   },
   reducers: {
     setNoteList: (currentSlice, action) => {
       currentSlice.noteList = action.payload;
     },
     addNote: (currentSlice, action) => {
-      currentSlice.noteList.push(action.payload as any);
+      currentSlice.noteList.push(action.payload as NoteType);
     },
     updateNote: (currentSlice, action) => {
       const indexToUpdate = currentSlice.noteList.findIndex(
