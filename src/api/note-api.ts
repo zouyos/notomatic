dbc2b49d22e742812076aef97c1f0f6fce88baf0
zouyos <hamza.benketaf@gmail.com>
2024-11-05 10:29:56 +0100

    note-api refactor

--- src/api/note-api.ts
+++ src/api/note-api.ts
@@ -49,7 +49,11 @@ export class NoteAPI {
           withCredentials: true,
         }
       );
-      return response.data.map(this.formatId)
+      if (Array.isArray(response.data)) {
+        return response.data.map(this.formatId);
+      } else {
+        throw new Error("Expected an array but got something else");
+      }
     } catch (err) {
       throw err;
     }
